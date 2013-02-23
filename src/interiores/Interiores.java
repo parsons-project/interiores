6bb1f3b0247d6eb136b263869df2d841796c0932
Héctor <hector0193@gmail.com>
2013-02-23 19:04:58 +0100

    NetBeans project created.

---
+++ src/interiores/Interiores.java
@@ -0,0 +1,19 @@
+/*
+ */
+package interiores;
+
+/**
+ *
+ * @author hector
+ */
+public class Interiores
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+}

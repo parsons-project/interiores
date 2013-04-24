71c425b462d61ce67073acccad0e3487954543b8
Lorenzo Arribas <lorenzo.arribas@me.com>
2013-04-24 17:33:40 +0200

    Starting to develop constraints

---
+++ src/interiores/business/models/FurnitureModel.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interiores.business.models;
+
+/**
+ *
+ * @author larribas
+ */
+public class FurnitureModel {
+    
+    
+    
+}

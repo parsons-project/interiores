b04637724a91f7503700510fd36f7dfd952bbfea
Héctor <hector0193@gmail.com>
2013-04-24 17:06:55 +0200

    Using OrientedRectangle :D

--- src/interiores/presentation/swing/views/map/Furniture.java
+++ src/interiores/presentation/swing/views/map/Furniture.java
@@ -1,9 +1,11 @@
 package interiores.presentation.swing.views.map;
 
+import interiores.business.models.Orientation;
+import interiores.business.models.OrientedRectangle;
+import interiores.core.Debug;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
-import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
 
 /**
@@ -13,23 +15,16 @@ import java.awt.geom.Rectangle2D;
 public class Furniture implements Drawable
 {
     private String name;
-    private int width;
-    private int height;
-    private int x;
-    private int y;
     private Color color;
-    private char orientation;
+    private OrientedRectangle rectangle;
     
     public Furniture(String name, int x, int y, int width, int height)
     {
         this.name = name;
-        this.width = width;
-        this.height = height;
-        this.x = x;
-        this.y = y;
-        
         color = Color.cyan;
-        orientation = 'D';
+        
+        rectangle = new OrientedRectangle(x  + GridMap.getPadding(), y + GridMap.getPadding(), width, height,
+                Orientation.S);
     }
     
     public void setColor(String c)
@@ -37,20 +32,19 @@ public class Furniture implements Drawable
         color = Color.decode(c);
     }
     
-    public void setOrientation(char o)
+    public void setOrientation(Orientation o)
     {
-        orientation = o;
+        rectangle.setOrientation(o);
     }
     
     @Override
     public void draw(Graphics2D g)
     {
-        Rectangle2D f = getRectangle();
         g.setColor(color);
-        g.fill(f);
+        g.fill(rectangle);
         
         g.setColor(Color.black);
-        g.draw(f);
+        g.draw(rectangle);
         
         Rectangle2D l = getOrientationMark();
         g.setColor(Color.black);
@@ -60,63 +54,43 @@ public class Furniture implements Drawable
         
         int stringW = (int) r.getWidth();
         int stringH = (int) r.getHeight();
-        int startW = ((int)f.getWidth())/2 - stringW/2;
-        int startH = ((int)f.getHeight())/2 + stringH/2;
+        int startW = ((int)rectangle.getWidth())/2 - stringW/2;
+        int startH = ((int)rectangle.getHeight())/2 + stringH/2;
         
         g.setColor(Color.black);
-        g.drawString(name, (int)f.getX() + startW, (int)f.getY() + startH);
-        
-    }
-    
-    private Rectangle2D getRectangle()
-    {
-        Rectangle2D r;
-        
-        switch(orientation)
-        {
-            case 'L':
-            case 'R':
-                r = new Rectangle(x + GridMap.getPadding(), y + GridMap.getPadding(), height, width);
-                break;
-                
-            case 'U':
-            default:
-                r = new Rectangle(x + GridMap.getPadding(), y + GridMap.getPadding(), width, height);
-        }
-        
-        return r;
+        g.drawString(name, (int)rectangle.getX() + startW, (int)rectangle.getY() + startH);
     }
     
     private Rectangle2D getOrientationMark()
     {
         int rW, rH;
-        int rX = 0;
-        int rY = 0;
+        double rX = 0;
+        double rY = 0;
         
-        switch(orientation)
+        switch(rectangle.getOrientation())
         {
-            case 'R':
-                rX = height;
+            case E:
+                rX = rectangle.getWidth();
            
-            case 'L':
-                rX += x + GridMap.getPadding() - 3;
-                rY = y + GridMap.getPadding() + width/2 - 1;
+            case W:
+                rX += rectangle.getX() - 3;
+                rY = rectangle.getY() + rectangle.getHeight() / 2 - 1;
                 
                 rW = 6;
                 rH = 1;
                 break;
                 
-            case 'D':
-                rY = height;
+            case S:
+                rY = rectangle.getHeight();
                 
             default:
-                rX = x + GridMap.getPadding() + width/2;
-                rY += y + GridMap.getPadding() - 3;
+                rX = rectangle.getX() + rectangle.getWidth() / 2;
+                rY += rectangle.getY() - 3;
                 
                 rW = 1;
                 rH = 6;
         }
-        
-        return new Rectangle(rX, rY, rW, rH);
+        Debug.println(rX + " " + rY);
+        return new Rectangle((int) rX, (int) rY, rW, rH);
     }
 }

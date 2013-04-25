22df3c5133ce7705f7c958a3c49990efcffa9ea8
Lorenzo Arribas <lorenzo.arribas@me.com>
2013-04-25 15:06:44 +0200

    SizeConstraint

--- src/interiores/business/models/SizeConstraint.java
+++ src/interiores/business/models/SizeConstraint.java
@@ -4,20 +4,54 @@
  */
 package interiores.business.models;
 
-import interiores.utils.Size;
+import java.awt.Dimension;
 
 /**
- *
+ * SizeConstraint represents a constraint imposed over the size of a furniture model.
  * @author larribas
  */
 public class SizeConstraint {
     
-    Size minSize, maxSize;
+    // minSize and maxSize determine the range within which the model's size should fall
+    private Dimension minSize, maxSize;
     
-    public boolean isSatisfied(FurnitureModel fm) {
-        Size modelSize = fm.getSize();
-        return modelSize.isLargerThan(minSize) && fm.getSize().isSmallerThan(maxSize);
+    /**
+     * Default constructor. Dimensions are initialized to zero.
+     */
+    public SizeConstraint() {
+        
     }
     
+    /**
+     * Builds a SizeConstraint with specific minimum and maximum sizes
+     * @param min The smallest size a model should have so as to satisfy the constraint
+     * @param max The largest size a model should have so as to satisfy the constraint
+     */
+    public SizeConstraint(Dimension min, Dimension max) {
+        minSize = min;
+        maxSize = max;
+    }
     
+    /**
+     * Determines whether a furniture model satisfies the constraint.
+     * This occurs when the model size falls within the specified range.
+     * For flexibility matters, a height or width of 0 is considered not to be a constraint
+     * @param model The model whose size will be checked.
+     * @return true if the model satisfies the size constraint; false otherwise
+     */
+    public boolean isSatisfied(FurnitureModel model) {
+        Dimension modelSize = model.getSize();
+        
+        // In order to check whether the model satisfies the constraint, height and width components
+        // are verified separately. If the maximum of a component is 0, it is considered not to
+        // represent a constraint, and thus any value of that component will validate the constraint.
+        boolean heightConstraint = maxSize.height == 0 ||
+                                   minSize.height <= modelSize.height && modelSize.height <= maxSize.height;
+        
+        boolean widthConstraint = maxSize.width == 0 ||
+                                   minSize.width <= modelSize.width && modelSize.width <= maxSize.width;
+        
+        return heightConstraint && widthConstraint;        
+    }
+        
 }

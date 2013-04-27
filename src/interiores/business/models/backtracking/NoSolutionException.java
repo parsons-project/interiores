f548036f641933f91bd8cf99a0439048917358cf
nmamano <tehotserver@gmail.com>
2013-04-27 19:57:10 +0200

    local commit

--- src/interiores/business/models/backtracking/NoSolutionException.java
+++ src/interiores/business/models/backtracking/NoSolutionException.java
@@ -5,21 +5,24 @@
 */
 public class NoSolutionException extends Exception
 {
-/**
-* Creates a NoSolutionException with a 'message ' providing
-* additional details .
-* @param message String containing the message of the exception .
-*/
-public NoSolutionException ( String message ) {
-super ( message );
-}
-/**
-* Constructs a new NoSolutionException with the specified
-* detail message and cause .
-* @param message String containing the message of the exception
-* @param cause Throwable cause of the exception .
-*/
-public NoSolutionException ( String message , Throwable cause ) {
-super ( message , cause );
-}
+    
+    /**
+    * Creates a NoSolutionException with a 'message ' providing
+    * additional details .
+    * @param message String containing the message of the exception .
+    */
+    public NoSolutionException (String message) {
+        super (message);
+    }
+    
+    
+    /**
+    * Constructs a new NoSolutionException with the specified
+    * detail message and cause .
+    * @param message String containing the message of the exception
+    * @param cause Throwable cause of the exception .
+    */
+    public NoSolutionException (String message, Throwable cause) {
+        super (message, cause);
+    }
 }

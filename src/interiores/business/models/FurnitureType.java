e9fd40e5a26baf8bb903983b901b893ac3e3bb0e
aespuna <aesbu@computer>
2013-04-28 00:23:44 +0200

    Added some documentation and method to check Dimensions (to be reviewed)

--- src/interiores/business/models/FurnitureType.java
+++ src/interiores/business/models/FurnitureType.java
@@ -4,7 +4,7 @@ package interiores.business.models;
 import java.awt.Dimension;
 
 /**
- * 
+ * This class represents the type of a furniture model
  * @author alvaro
  */
 public class FurnitureType {
@@ -26,16 +26,40 @@ public class FurnitureType {
         this.maximum = max;
     }
     
+    /**
+     * Gets the name of the type
+     * @return The name of the type of furniture
+     */
     public String getName() {
         return name;
     }
     
+    /**
+     * Gets the minimum dimensions a furniture model of this type should have
+     * @return The minimum dimensions a furniture model of this type should have
+     */
     public Dimension getMinDimension() {
         return minimum;
     }
     
+    /**
+     * Gets the maximum dimensions a furniture model of this type should have
+     * @return The maximum dimensions a furniture model of this type should have
+     */
     public Dimension getMaxDimension() {
         return maximum;
+    }
+    
+    
+    // should this go here or maybe in the controller?
+    /**
+     * Checks if dimension is between the minimum and the maximum dimensions of the type
+     * @param dimension The dimension to be checked
+     * @return True if the dimension is between the minimum and the maximum dimensions of the type, False otherwise
+     */
+    public Boolean checkDimension(Dimension dimension) {
+        return (minimum.width <= dimension.width && dimension.width <= maximum.width) &&
+               (minimum.height <= dimension.height && dimension.height <= maximum.height);
     }
     
 }

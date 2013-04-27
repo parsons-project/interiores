8b402221bd127ff310547a25219c3464d28353c3
Héctor <hector0193@gmail.com>
2013-04-27 17:43:55 +0200

    NoSolutionException completed.

--- src/interiores/shared/backtracking/NoSolutionException.java
+++ src/interiores/shared/backtracking/NoSolutionException.java
@@ -1,11 +1,37 @@
 package interiores.shared.backtracking;
 
 /**
- *
- * @author hector
- */
+* NoSolutionException represents an exception generated when
+* the algorithm isn't able to find a set of values that
+* matches the variables, such that no restriction is violated
+* @author nil.mamano
+*/
 public class NoSolutionException
     extends Exception
 {
-
+    /**
+     * Generic NoSolutionException constructor.
+     */
+    public NoSolutionException() {
+        super();
+    }
+    
+    /**
+    * Creates a NoSolutionException with a 'message' providing
+    * additional details.
+    * @param message String containing the message of the exception.
+    */
+    public NoSolutionException(String message) {
+        super(message);
+    }
+    
+    /**
+    * Constructs a new NoSolutionException with the specified
+    * detail message and cause.
+    * @param message String containing the message of the exception
+    * @param cause Throwable cause of the exception.
+    */
+    public NoSolutionException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }

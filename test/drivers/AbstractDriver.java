65a7831caf0b7d5bacdce724d40f387e362565c9
aesbu <aesbu@fib>
2013-04-29 20:29:24 +0200

    More reading methods added

--- test/drivers/AbstractDriver.java
+++ test/drivers/AbstractDriver.java
@@ -3,10 +3,13 @@ package drivers;
 import interiores.business.models.FurnitureModel;
 import interiores.utils.Dimension;
 import interiores.business.models.FurnitureType;
+import interiores.business.models.RoomType;
 import interiores.core.presentation.terminal.IOStream;
 import java.awt.Color;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  *
@@ -16,8 +19,15 @@ public abstract class AbstractDriver {
     
     private static IOStream iostream = new IOStream(System.in, System.out);
     
+    public Dimension readDimension() {
+        int w = iostream.readInt("Enter the width: ");
+        int h = iostream.readInt("Enter the depth: ");
+        
+        return new Dimension(w, h);
+    }
+    
     public Color readColor() {
-        iostream.println("Introduce un color");
+        
         int r = iostream.readInt("R:");
         int g = iostream.readInt("G: ");
         int b = iostream.readInt("B: ");
@@ -29,14 +39,13 @@ public abstract class AbstractDriver {
         
         String name = iostream.readString("Enter the name of the furniture type: ");
         
-        int minWidth = iostream.readInt("Enter the minimum width of the type: ");
-        int minDepth = iostream.readInt("Enter the minimum depth of the type: ");
+        iostream.println("Enter the minimum dimensions of the furniture model: ");
+        Dimension minDim = readDimension();
         
-        int maxWidth = iostream.readInt("Enter the maximum width of the type: ");
-        int maxDepth = iostream.readInt("Enter the maximum depth of the type: ");
-        
-        return new FurnitureType(name, new Dimension(minWidth, minDepth),
-                                 new Dimension(maxWidth, maxDepth));
+        iostream.println("Enter the maximum dimensions of the furniture model: ");
+        Dimension maxDim = readDimension();
+                
+        return new FurnitureType(name, minDim, maxDim);
     }
     
     public FurnitureModel readFurnitureModel() {
@@ -45,35 +54,44 @@ public abstract class AbstractDriver {
         
         String name = iostream.readString("Enter the name of the furniture model: ");
         
-        int width = iostream.readInt("Enter the minimum width of the model: ");
-        int depth = iostream.readInt("Enter the minimum depth of the model: ");
+        iostream.println("Enter the dimensions of the model: ");
+        Dimension dim = readDimension();
         
         float price = iostream.readFloat("Enter the price of the furniture model: ");
         
+        iostream.println("Introduce un color");
         Color color = readColor();
         
         String material = iostream.readString("Enter the material of the furniture model");
         
         
-        return new FurnitureModel(ftype, name, Dimension(width, depth), price, color, material);
+        return new FurnitureModel(ftype, name, dim, price, color, material);
         
     }
     
-    public abstract readInput(String filename);
-        BufferedReader br = new BufferedReader(new FileReader(filename));
-        try {
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
-
-            while (line != null) {
-                sb.append(line);
-                sb.append("\n");
-                line = br.readLine();
-            }
-            String everything = sb.toString();
-        } finally {
-            br.close();
-       
+    public Collection<FurnitureType> readFurnitureTypeCollection() {
+        
+        int n = iostream.readInt("How many?: ");
+        Collection<FurnitureType> furnitureTypes = new ArrayList<FurnitureType>(n);
+                
+        for (FurnitureType ft : furnitureTypes) {
+            ft = readFurnitureType();
+        }
+        
+        return furnitureTypes;
+    }
+    
+    public RoomType readRoomType() {
+        
+        String name = iostream.readString("Enter the name of the room type: ");
+        
+        iostream.println("Enter the furnitures that have to be in this type of Room: ");
+        Collection<FurnitureType> accepted = readFurnitureTypeCollection();
+        
+        iostream.println("Enter the furnitures that can't to be in this type of Room: ");
+        Collection<FurnitureType> forbidden = readFurnitureTypeCollection();
+        
+        return new RoomType(name, accepted, forbidden);
     }
     
     

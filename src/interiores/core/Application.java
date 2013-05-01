a1c34b8d9ae80053ececb2a313687982ad0e69b7
Héctor <hector0193@gmail.com>
2013-05-01 18:52:11 +0200

    core.Application documented

--- src/interiores/core/Application.java
+++ src/interiores/core/Application.java
@@ -7,21 +7,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ * Application represents a Java application using the 3-tier design pattern.
  * @author hector
  */
 public class Application
 {
+    /**
+     * Application package used to find the business controllers
+     */
     private String appPkg;
+    
+    /**
+     * The controller of the application data layer
+     */
     private JAXBDataController data;
+    
+    /**
+     * The controllers of the application presentation layer
+     */
     private List<PresentationController> presentations;
     
+    /**
+     * Application constructor.
+     * @param appPkg Application package used to find the business controllers
+     */
     public Application(String appPkg)
     {
         this.appPkg = appPkg;
         presentations = new ArrayList();
     }
     
+    /**
+     * Initializes the application.
+     * This method basically initializes the presentation layer which can start making requests to the
+     * business.
+     */
     public void init()
     {
         try {
@@ -34,10 +54,29 @@ public class Application
         }
     }
     
+    /**
+     * Sets the controller of the data layer.
+     * This data controller is injected to the business controllers on creation.
+     * @param data The controller of the data layer
+     */
     public void setDataController(JAXBDataController data) {
         this.data = data;
     }
     
+    /**
+     * Adds a presentation controller to the presentation layer.
+     * @param presentation The presentation controller
+     */
+    public void addPresentation(PresentationController presentation)
+    {
+        presentations.add(presentation);
+    }
+    
+    /**
+     * Uses reflection to add business controllers to the business layer dinamically!
+     * Every controller is injected with the data controller when constructed.
+     * @param name Name of the business controller (camel cased)
+     */
     public void addBusiness(String name)
     {
         try
@@ -55,14 +94,14 @@ public class Application
         }
     }
     
+    /**
+     * Assigns a name to a controller in the business layer without reflection.
+     * @param name Name to identify the controller
+     * @param controller The business controller
+     */
     public void addBusiness(String name, BusinessController controller)
     {
         for(PresentationController presentation : presentations)
             presentation.addBusinessController(name, controller);
-    }
-    
-    public void addPresentation(PresentationController presentation)
-    {
-        presentations.add(presentation);
     }
 }

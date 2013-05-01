e340ed0b4c0412fdf7b5650c8b1cc56fd6496d45
Héctor <hector0193@gmail.com>
2013-05-01 19:53:20 +0200

    interiores.core commented

--- src/interiores/core/Debug.java
+++ src/interiores/core/Debug.java
@@ -1,21 +1,35 @@
 package interiores.core;
 
 /**
- *
+ * Simple class for enable/disable debugging mesages.
  * @author hector
  */
 public class Debug {
+    /**
+     * Tells whether the debug mode is enabled or not.
+     */
     private static boolean enabled = false;
     
+    /**
+     * Enables the debug mode.
+     */
     public static void enable() {
         enabled = true;
     }
     
+    /**
+     * If the debug mode is enabled, prints as a line the given string.
+     * @param s The string to print
+     */
     public static void println(String s) {
         if(enabled)
             System.err.println("[DEBUG] " + s);
     }
     
+    /**
+     * Tells whether the debug mode is enabled or not
+     * @return Ture if the debug mode is enabled, false otherwise
+     */
     public static boolean isEnabled() {
         return enabled;
     }
--- src/interiores/core/Observer.java
+++ src/interiores/core/Observer.java
@@ -3,7 +3,8 @@ package interiores.core;
 import java.util.Map;
 
 /**
- *
+ * All the instances that implement this interface can recieve notifications from Observable instances.
+ * The Observer interface is part of the observer pattern.
  * @author hector
  */
 public interface Observer

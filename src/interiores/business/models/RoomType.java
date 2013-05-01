1041d85de6b0fe0d3e073f13991f227417df6d5b
nmamano <nil.mamano@est.fib.upc.edu>
2013-05-01 14:40:57 +0200

    Documentation added

--- src/interiores/business/models/RoomType.java
+++ src/interiores/business/models/RoomType.java
@@ -32,11 +32,24 @@ public class RoomType
         this(null);
     }
     
+    /**
+     * Simple creator of the room type
+     * @param name The name of the room type
+     */
     public RoomType(String name) {
         this(name, new ArrayList(), new ArrayList());
     }
     
-    public RoomType(String name, Collection<FurnitureType> mustHave, Collection<FurnitureType> cantHave) {
+    /**
+     * Full creator of the room type
+     * @param name The name of the room type
+     * @param mustHave A collection of the furniture types that a room of this type
+     *                 must have
+     * @param cantHave A collection of the furniture types that a room of this type
+     *                 cannot contain
+     */    
+    public RoomType(String name, Collection<FurnitureType> mustHave,
+                    Collection<FurnitureType> cantHave) {
         super(name);
         this.mustHave = new HashSet();
         this.cantHave = new HashSet();
@@ -48,47 +61,96 @@ public class RoomType
             this.cantHave.add(fType.getId());
     }
     
+    /**
+     * Get the identifier of the room type
+     * @return The name of the room type
+     */
     public String getName() {
         return identifier;        
     }
     
+    /**
+     * Get a hash set of the names of the funiture types that this type of room must contain. 
+     * @return A hash set of the names of the funiture types that this type of room must contain.
+     */
     public HashSet<String> getMandatory() {
         return mustHave;
     }
     
+    /**
+     * Adds a furniture type to the set of the mandatory furniture types it must contain
+     * @param fType The furniture type to add to the mandatory set 
+     */
     public void addToMandatory(FurnitureType fType) {
         mustHave.add(fType.getId());
     }
     
+    /**
+     * Removes the furniture type of the set of the mandatory furniture types. If it wasn't
+     * present it is ignored. Either way the set will not contain the type.
+     * @param fType The furniture type to be removed 
+     */
     public void removeFromMandatory(FurnitureType fType) {
         cantHave.remove(fType.getId());
     }
     
+    /**
+     * Removes the furniture type of the set of the mandatory furniture types. If it wasn't
+     * present it is ignored. Either way the set will not contain the type.
+     * @param fTypename The furniture type name to be removed 
+     */
     public void removeFromMandatory(String fTypename) {
         cantHave.remove(fTypename);
     }
     
+    /**
+     * Get a hash set of the names of the funiture types that this type of room can't contain. 
+     * @return A hash set of the names of the funiture types that this type of room can't contain.
+     */
     public HashSet<String> getForbidden() {
         return cantHave;
     }
     
+    /**
+     * Adds a furniture type to the set of the mandatory furniture types it can't contain
+     * @param fType The furniture type to add to the forbidden set
+     */
     public void addToForbidden(FurnitureType fType) {
         cantHave.add(fType.getId());
     }
     
+    /**
+     * Removes the furniture type of the set of the forbidden furniture types. If it wasn't
+     * present it is ignored. Either way the set will not contain the type.
+     * @param fTypename The furniture type name to be removed 
+     */
     public void removeFromForbidden(FurnitureType fType) {
         cantHave.remove(fType.getId());
     }
     
+     /**
+     * Removes the furniture type of the set of the forbidden furniture types. If it wasn't
+     * present it is ignored. Either way the set will not contain the type.
+     * @param fTypename The furniture type name to be removed 
+     */
     public void removeFromForbidden(String fTypename) {
         cantHave.remove(fTypename);
     }
     
-    // should these methods go here or maybe in the controller
+    /**
+     * Checks if a fueniture type is mandatory or not
+     * @param ftype The furniture type to be checked
+     * @return true if ftype is in the mandatory set, false otherwise
+     */
     public boolean isMandatory(FurnitureType ftype) {
         return mustHave.contains(ftype.getId());
     }
     
+     /**
+     * Checks if a fueniture type is forbidden or not
+     * @param ftype The furniture type to be checked
+     * @return true if ftype is in the forbidden set, false otherwise
+     */
     public boolean isForbidden(FurnitureType ftype) {
         return cantHave.contains(ftype.getId());
     }

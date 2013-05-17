96eb81743d2d1caaa98861668090141f01efddc7
Lorenzo Arribas <lorenzo.arribas@me.com>
2013-05-17 09:54:06 +0200

    Same material contraint aded.

--- src/interiores/business/models/constraints/global/SameMaterialConstraint.java
+++ src/interiores/business/models/constraints/global/SameMaterialConstraint.java
@@ -8,50 +8,46 @@ import interiores.business.exceptions.ConstraintException;
 import interiores.business.models.backtracking.Domain;
 import interiores.business.models.backtracking.FurnitureValue;
 import interiores.business.models.constraints.GlobalConstraint;
-import java.awt.Color;
 import java.util.List;
 
 /**
- * Represents a global constraint imposed over the color of all the elements in a room.
- * This constraint forces all the furniture to be the same color, regardless of which 
- * specific color it is
+ * Represents a global constraint imposed over the material of all the elements in a room.
+ * This constraint forces all the furniture to be made in the same material, regardless 
+ * of which specific material it is
  * @author larribas
  */
-public class SameColorConstraint extends GlobalConstraint {
+public class SameMaterialConstraint extends GlobalConstraint {
 
     private int assignments = 0;
     
-    private Color current_color;
+    private String current_material;
     
-    public SameColorConstraint() { }
-    
-
     @Override
     public void notifyAssignment(FurnitureValue fv) throws ConstraintException {
         
-        if (assignments == 0) current_color = fv.getModel().getColor();
-        else if ( current_color != fv.getModel().getColor() )
-            throw new ConstraintException("Trying to assign a " + fv.getModel().getColor().toString()
-                    + " furniture to a room which can only contain " + current_color.toString() + " ones.");
+        if (assignments == 0) current_material = fv.getModel().getMaterial();
+        else if ( ! current_material.equals(fv.getModel().getMaterial()) )
+            throw new ConstraintException("Trying to assign a furniture made in " + fv.getModel().getMaterial()
+                    + " to a room whose furniture should be made in " + current_material);
         
         assignments++;
+        
     }
 
     @Override
     public void notifyUnassignment(FurnitureValue fv) {
-        
         assignments--;
     }
 
     @Override
     public void eliminateInvalidValues(List<Domain> domains) {
         
-        // TODO Eliminate invalid values (due to the current color)
+        // TODO Eliminate invalid values (due to the current material)
         // Pending implementation. This method is intended to, for instance,
         // eliminate all the models of unassigned variables such that their
-        // color is different from the current color (as long as assigned > 0).
+        // material is different from the current material (as long as assigned > 0).
         
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }

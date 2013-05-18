36a1f04be39f4738103604750cc76e0b6e31173c
Lorenzo Arribas <lorenzo.arribas@me.com>
2013-05-18 18:34:40 +0200

    Timed test added

--- test/interiores/business/models/backtracking/StageAltTest.java
+++ test/interiores/business/models/backtracking/StageAltTest.java
@@ -15,6 +15,7 @@ import java.awt.Rectangle;
 import java.awt.geom.Area;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -75,4 +76,50 @@ public class StageAltTest {
                 
         System.out.println("........... THE END");
     }
+    
+    /**
+     * Test of getNextDomainValue method, of class StageAlt.
+     */
+    @Test
+    public void testGetNextDomainValueTimed() {
+        System.out.println("Testing has/get-NextDomainValue() for TIME .........");
+        
+        // PARAMETERS
+        int nb_models = 50; // The number of models
+        // The resolution (should be changed directly on ExtendedArea)
+        Dimension dim = new Dimension(1000,1000); // The size of the room
+        
+        // Quite the worst-case scenario for a single variable domain
+        Area room = new Area(new Rectangle(0, 0, dim.width, dim.depth));
+        
+        // And a basic list of furniture models
+        List<FurnitureModel> l = new ArrayList();
+        for (int i = 0; i < nb_models; i++) { 
+            Random rand = new Random();
+            Dimension d = new Dimension(20 + rand.nextInt(80),20 + rand.nextInt(80));
+            FurnitureModel fm = new FurnitureModel("m"+i,d,10,Color.WHITE,"wood");
+            l.add(fm);
+            System.out.println("Added model " + fm.toString());
+        }
+            
+        // We create the stage
+        StageAlt instance = new StageAlt(l,room);
+            
+        long startTime = System.nanoTime();
+        
+        // And start going through the values in the domain.
+        // Now, note that even this simple example is complex in the current context.
+        // This test, thus, only serves the purpose of verifying whether the methods
+        // are consistent and do not throw any kind of exception
+        while (instance.hasMoreValues()) {
+            FurnitureValue fv = (FurnitureValue) instance.getNextDomainValue();
+        }
+        
+        long endTime = System.nanoTime();
+
+        long duration = endTime - startTime;
+                
+        System.out.println("........... THE END ........ elapsed " + duration + "nanoseconds.");
+    }
+        
 }

24da1a85b5d53f148e26fa522fbdd1953ceabad8
Lorenzo Arribas <lorenzo.arribas@me.com>
2013-05-22 20:55:56 +0200

    Catalog selection permitted

--- src/interiores/presentation/swing/views/RoomTypeCatalogPanel.java
+++ src/interiores/presentation/swing/views/RoomTypeCatalogPanel.java
@@ -9,11 +9,14 @@ import interiores.business.controllers.RoomTypesCatalogController;
 import interiores.business.models.RoomType;
 import interiores.core.Debug;
 import interiores.core.presentation.SwingController;
+import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 
 /**
  *
@@ -39,7 +42,9 @@ public class RoomTypeCatalogPanel extends javax.swing.JPanel {
         
         catElements = new ArrayList();
         
-        initCatalog();
+        // Initially, we load the current catalog
+        loadCatalog();
+        initCatalogList();
     }
 
     /**
@@ -53,36 +58,56 @@ public class RoomTypeCatalogPanel extends javax.swing.JPanel {
         title1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jPanel1 = new javax.swing.JPanel();
+        currentCatalogSelect = new javax.swing.JComboBox();
+
+        setBackground(new java.awt.Color(255, 255, 255));
+        setBounds(new java.awt.Rectangle(0, 0, 900, 600));
+        setMaximumSize(new java.awt.Dimension(900, 600));
+        setMinimumSize(new java.awt.Dimension(900, 600));
+        setPreferredSize(new java.awt.Dimension(900, 600));
 
         title1.setFont(new java.awt.Font("Lucida Grande", 0, 18)); // NOI18N
         title1.setText("Room types catalog");
         title1.setVerticalAlignment(javax.swing.SwingConstants.TOP);
 
+        jScrollPane1.setBorder(null);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setAutoscrolls(true);
         jPanel1.setLayout(new javax.swing.BoxLayout(jPanel1, javax.swing.BoxLayout.PAGE_AXIS));
         jScrollPane1.setViewportView(jPanel1);
+
+        currentCatalogSelect.setBackground(new java.awt.Color(255, 255, 255));
+        currentCatalogSelect.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(jScrollPane1)
             .add(layout.createSequentialGroup()
                 .add(68, 68, 68)
                 .add(title1)
-                .addContainerGap(601, Short.MAX_VALUE))
+                .add(18, 18, 18)
+                .add(currentCatalogSelect, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(547, Short.MAX_VALUE))
+            .add(layout.createSequentialGroup()
+                .add(jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .add(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(layout.createSequentialGroup()
                 .add(56, 56, 56)
-                .add(title1)
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(title1)
+                    .add(currentCatalogSelect, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
                 .add(18, 18, 18)
-                .add(jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 178, Short.MAX_VALUE))
+                .add(jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox currentCatalogSelect;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel title1;
@@ -90,7 +115,7 @@ public class RoomTypeCatalogPanel extends javax.swing.JPanel {
 
     
 
-    public void initCatalog() {
+    public void loadCatalog() {
         
         // Retrieve all the elements in the catalog
         Map<String,String> rtypes = rtController.getFullNamesMap();
@@ -115,12 +140,18 @@ public class RoomTypeCatalogPanel extends javax.swing.JPanel {
 //                    + "name: " + key + ", width: " + width + ", depth: " + depth
 //                    + "\nMandatory: " + mandatory + "\nForbidden: " + forbidden);
             
-        }
-       
-        
+        }       
+    }
+    
+    public void initCatalogList() {
+        Collection<String> catalogs = rtcController.getNamesLoadedCatalogs();
+        Object[] s = catalogs.toArray();
+        currentCatalogSelect.setModel(new javax.swing.DefaultComboBoxModel(s) );
     }
 
+    
 
+    // This class represents a catalog element
     class RTC_Element {
         private javax.swing.JPanel outerPanel = new javax.swing.JPanel();
         private javax.swing.JButton removeButton = new javax.swing.JButton();
@@ -136,16 +167,25 @@ public class RoomTypeCatalogPanel extends javax.swing.JPanel {
         private javax.swing.JLabel measureLabel2 = new javax.swing.JLabel();
         private javax.swing.JLabel measureLabel3 = new javax.swing.JLabel();
         
-        public RTC_Element(String key, Integer width, Integer depth, String mandatory, String forbidden) {
-            removeButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/remove_element.png"))); // NOI18N
-
+        public RTC_Element(String rname, Integer width, Integer depth, String mandatory, String forbidden) {
+            
+            ImageIcon im = new javax.swing.ImageIcon(getClass().getResource("/resources/remove_element.png"));
+            im.setImage( im.getImage().getScaledInstance(40,40,java.awt.Image.SCALE_SMOOTH) );
+            removeButton.setIcon(im); // NOI18N
+            removeButton.setBorder(BorderFactory.createEmptyBorder());
+            removeButton.setContentAreaFilled(false);
+            
+            
             innerPanel.setBackground(new java.awt.Color(255, 255, 255));
             innerPanel.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(204, 204, 204)));
-            innerPanel.setMaximumSize(new java.awt.Dimension(650, 125));
-            innerPanel.setMinimumSize(new java.awt.Dimension(650, 125));
+            
+            outerPanel.setBackground(new java.awt.Color(255, 255, 255));
+            outerPanel.setSize(new Dimension(800, 200));
+            outerPanel.setMinimumSize(new Dimension(800, 200));
+            outerPanel.setMaximumSize(new Dimension(800, 200));
 
             rtname.setFont(new java.awt.Font("Lucida Grande", 0, 16)); // NOI18N
-            rtname.setText(key + ":");
+            rtname.setText(rname + ":");
 
             measureLabel1.setText("This room should measure more than");
             measureField1.setText(width.toString());
@@ -160,8 +200,10 @@ public class RoomTypeCatalogPanel extends javax.swing.JPanel {
             measureLabel3.setText("cm deep");
             mandatoryLabel.setText("Mandatory furniture:");
             mandatoryField.setText(mandatory);
+            mandatoryField.setColumns(30);
             forbiddenLabel.setText("Forbidden furniture:");
             forbiddenField.setText(forbidden);
+            forbiddenField.setColumns(30);
             
 
             org.jdesktop.layout.GroupLayout innerPanelLayout = new org.jdesktop.layout.GroupLayout(innerPanel);
@@ -214,13 +256,12 @@ public class RoomTypeCatalogPanel extends javax.swing.JPanel {
                         .add(forbiddenLabel))
                     .addContainerGap(15, Short.MAX_VALUE))
             );
-
+            
             org.jdesktop.layout.GroupLayout outerPanelLayout = new org.jdesktop.layout.GroupLayout(outerPanel);
             outerPanel.setLayout(outerPanelLayout);
             outerPanelLayout.setHorizontalGroup(
                 outerPanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                .add(outerPanelLayout.createSequentialGroup()
-                    .add(37, 37, 37)
+                    .add(outerPanelLayout.createSequentialGroup()
                     .add(removeButton, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 31, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
                     .addContainerGap(768, Short.MAX_VALUE))
                 .add(outerPanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
@@ -231,10 +272,11 @@ public class RoomTypeCatalogPanel extends javax.swing.JPanel {
             );
             outerPanelLayout.setVerticalGroup(
                 outerPanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                .add(0, 0, Short.MAX_VALUE)
                 .add(outerPanelLayout.createSequentialGroup()
                     .add(26, 26, 26)
                     .add(removeButton, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 32, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(116, Short.MAX_VALUE))
+                    .addContainerGap(20, Short.MAX_VALUE))
                 .add(outerPanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
                     .add(outerPanelLayout.createSequentialGroup()
                         .add(0, 24, Short.MAX_VALUE)

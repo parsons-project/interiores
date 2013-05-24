218a024d7b0cadaf56607ec8c8baa1984715186f
Héctor <hector0193@gmail.com>
2013-05-24 12:43:08 +0200

    Overwrite file message changed.

--- src/interiores/presentation/swing/helpers/FileChooser.java
+++ src/interiores/presentation/swing/helpers/FileChooser.java
@@ -16,8 +16,8 @@ public class FileChooser
         File f = getSelectedFile();
         
         if(f.exists() && getDialogType() == SAVE_DIALOG) {
-            int result = JOptionPane.showConfirmDialog(this, "The file exists, overwrite?",
-                    "Existing file", JOptionPane.YES_NO_CANCEL_OPTION);
+            int result = JOptionPane.showConfirmDialog(this, "The selected file already exists. Do you want to "
+                    + "overwrite?", "Existing file", JOptionPane.YES_NO_CANCEL_OPTION);
             switch(result) {
                 case JOptionPane.YES_OPTION:
                     super.approveSelection();

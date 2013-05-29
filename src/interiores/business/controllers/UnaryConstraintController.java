5c7772ed0b75a9f03b2290e0f9b2aef1e3200875
Lorenzo Arribas <lorenzo.arribas@me.com>
2013-05-29 23:20:26 +0200

    UCC documented

--- src/interiores/business/controllers/UnaryConstraintController.java
+++ src/interiores/business/controllers/UnaryConstraintController.java
@@ -20,7 +20,6 @@ import interiores.business.models.constraints.furniture.unary.WidthConstraint;
 import interiores.core.data.JAXBDataController;
 import java.awt.Point;
 import java.awt.Rectangle;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -54,6 +53,10 @@ public class UnaryConstraintController
         UnaryConstraint.addConstraintClass(name, uClass);
     }
     
+    /**
+     * Obtains all the available unary constraints to define
+     * @return A collection of strings containing the names the unary constraints are defined by
+     */
     public Collection<String> getAvailableConstraints() {
         return UnaryConstraint.getConstraintNames();
     }
@@ -67,59 +70,122 @@ public class UnaryConstraintController
         return getWishList().getUnaryConstraints(id);
     }
     
+    /**
+     * Adds a constraint imposing a range of valid widths over a wanted furniture
+     * @param furnitureId The wanted furniture the constraint is to be imposed over
+     * @param minWidth The minimum width
+     * @param maxWidth The maximum width
+     */
     public void addWidthConstraint(String furnitureId, int minWidth, int maxWidth) {
         addConstraint(furnitureId, new WidthConstraint(minWidth, maxWidth));
     }
     
-    public void addDepthConstraint(String furnitureId, int minWidth, int maxWidth) {
-        addConstraint(furnitureId, new DepthConstraint(minWidth, maxWidth));
+    /**
+     * Adds a constraint imposing a range of valid depths over a wanted furniture
+     * @param furnitureId The wanted furniture the constraint is to be imposed over
+     * @param minDepth The minimum depth
+     * @param maxDepth The maximum depth
+     */
+    public void addDepthConstraint(String furnitureId, int minDepth, int maxDepth) {
+        addConstraint(furnitureId, new DepthConstraint(minDepth, maxDepth));
     }
     
+    /**
+     * Adds a constraint imposing a specific color for a wanted furniture to be painted in
+     * @param furnitureId The wanted furniture the constraint is to be imposed over
+     * @param color The very color, represented by a name
+     */
     public void addColorConstraint(String furnitureId, String color) {
         addConstraint(furnitureId, new ColorConstraint(color));
     }
     
+    /**
+     * Adds a constraint imposing a specific material for a wanted furniture to be made from
+     * @param furnitureId The wanted furniture the constraint is to be imposed over
+     * @param material The very material, represented by a name
+     */
     public void addMaterialConstraint(String furnitureId, String material) {
         addConstraint(furnitureId, new MaterialConstraint(material));
     }
     
+    /**
+     * Adds a constraint selecting a specific model for a wanted furniture
+     * @param furnitureId The wanted furniture the constraint is to be imposed over
+     * @param modelName The name of the specific model
+     */
     public void addModelConstraint(String furnitureId, String modelName) {
         addConstraint(furnitureId, new ModelConstraint(modelName));
     }
     
+    /**
+     * Adds a constraint selecting the valid orientations a wanted furniture should be placed towards
+     * @param furnitureId The wanted furniture the constraint is to be imposed over
+     * @param orientation The valid orientation(s)
+     */
     public void addOrientationConstraint(String furnitureId, String orientation) {
         addConstraint(furnitureId, new OrientationConstraint(orientation));
     }
     
+    /**
+     * Adds a constraint imposing the maximum price a wanted furniture must cost
+     * @param furnitureId The wanted furniture the constraint is to be imposed over
+     * @param maxPrice The threshold price that floods the budget
+     */
     public void addPriceConstraint(String furnitureId, float maxPrice) {
         addConstraint(furnitureId, new PriceConstraint(maxPrice));
     }
     
+    /**
+     * Adds a constraint selecting a list of valid positions a wanted furniture can be placed at
+     * @param furnitureId The wanted furniture the constraint is to be imposed over
+     * @param validPositions A list of valid positions
+     */
     public void addPositionConstraint(String furnitureId, List<Point> validPositions) {
         throw new UnsupportedOperationException("Not supported yet.");
         // @TODO Redefine this method
     }
     
+    /**
+     * Adds a constraint selecting a specific position a determined wanted furniture must be placed at
+     * @param furnitureId The wanted furniture the constraint is to be imposed over
+     * @param x The x coordinate of such a position
+     * @param y The y coordinate of such a position
+     */
     public void addPositionAtConstraint(String furnitureId, int x, int y) {
         addConstraint(furnitureId, new PositionConstraint(x,y));
     }
     
+    /**
+     * Adds a constraint selecting a range of valid positions a wanted furniture can be placed at
+     * @param furnitureId The wanted furniture the constraint is to be imposed over
+     */
     public void addPositionRangeConstraint(String furnitureId, int x1, int y1, int x2, int y2) {
         Area area = new Area(new Rectangle(x1, y1, x2, y2));
         addConstraint(furnitureId, new AreaConstraint(area));
     }
     
+    /**
+     * Adds a constraint imposing a determined wanted furniture to be clung to some wall(s)
+     * @param furnitureId The wanted furniture the constraint is to be imposed over
+     * @param whichWalls The walls the furniture can be clung to
+     */
     public void addWallConstraint(String furnitureId, Orientation[] whichWalls) {
         Room room = getRoom();
         
         addConstraint(furnitureId, new WallConstraint(room.getWidth(), room.getDepth(), whichWalls));
     }
 
+    
     private void addConstraint(String furnitureId, UnaryConstraint unaryConstraint) {
         getWishList().addUnaryConstraint(furnitureId, unaryConstraint);
         notify(new UnaryConstraintAddedEvent());
     }
     
+    /**
+     * Removes a certain unary constrain imposed over a specified wanted furniture
+     * @param furnitureId The wanted furniture from which to remove the constraint
+     * @param unaryConstraintAlias The unary constraint to remove
+     */
     public void remove(String furnitureId, String unaryConstraintAlias) {
         Class<? extends UnaryConstraint> unaryConstraintClass;
         unaryConstraintClass = UnaryConstraint.getConstraintClass(unaryConstraintAlias);

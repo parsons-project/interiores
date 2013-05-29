0136d88aaeefd5a2d5b575ac3a01e0df5549b9fa
Lorenzo Arribas <lorenzo.arribas@me.com>
2013-05-29 23:06:11 +0200

    RTCC documented

--- src/interiores/business/controllers/RoomTypesCatalogController.java
+++ src/interiores/business/controllers/RoomTypesCatalogController.java
@@ -36,18 +36,33 @@ public class RoomTypesCatalogController
         setActiveCatalog(sessionCatalog);
     }
     
+    /**
+     * Creates a new room type catalog with a specified name
+     * @param catalogName The name the new room type catalog will have
+     */
     @Override
     public void create(String catalogName) {
         super.create(catalogName);
         notify(new RTCatalogSetModifiedEvent(catalogName,true));
     }
     
+    /**
+     * Removes the specified room type catalog from the list of available catalogs
+     * @param catalogName The name of the room type catalog to remove
+     */
     @Override
     public void remove(String catalogName) {
         super.remove(catalogName);
         notify(new RTCatalogSetModifiedEvent(catalogName, false));
     }
     
+    /**
+     * Merges a named furniture type catalog with the currently active one.
+     * If replacement is enabled, it replaces all the furniture types named
+     * identically, with the other catalog's ones
+     * @param catalogName The name of the other room type catalog
+     * @param replace Turns on or off the replacement option for the merge
+     */
     @Override
     public void merge(String catalogName, boolean replace) {
         super.merge(catalogName,replace);
@@ -57,12 +72,21 @@ public class RoomTypesCatalogController
         notify(new RTCatalogCheckoutEvent(getNameActiveCatalog()));
     }
     
+    /**
+     * Switches the active catalog to the one specified
+     * @param catalogName The name of the room type catalog to activate
+     */
     @Override
     public void checkout(String catalogName) {
         super.checkout(catalogName);
         notify(new RTCatalogCheckoutEvent(catalogName));
     }
     
+    /**
+     * Loads a room type catalog (previously saved) from disk
+     * @param path The absolute or relative directory path
+     * @throws JAXBException 
+     */
     @Override
     public void load(String path) throws JAXBException {
         super.load(path);

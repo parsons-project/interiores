c9e34373e760fc89ef1e37a4e706e51cf45baced
Lorenzo Arribas <lorenzo.arribas@me.com>
2013-05-29 22:58:50 +0200

    FTCC documented

--- src/interiores/business/controllers/FurnitureTypesCatalogController.java
+++ src/interiores/business/controllers/FurnitureTypesCatalogController.java
@@ -40,18 +40,33 @@ public class FurnitureTypesCatalogController
         setActiveCatalog(sessionCatalog);
     }
     
+    /**
+     * Creates a new furniture type catalog with a specified name
+     * @param catalogName The name of the new furniture type catalog
+     */
     @Override
     public void create(String catalogName) {
         super.create(catalogName);
         notify(new FTCatalogSetModifiedEvent(catalogName,true));
     }
     
+    /**
+     * Removes the named furniture type catalog
+     * @param catalogName The name of the furniture type catalog to delete
+     */
     @Override
     public void remove(String catalogName) {
         super.remove(catalogName);
         notify(new FTCatalogSetModifiedEvent(catalogName, false));
     }
     
+    /**
+     * Merges a named furniture type catalog with the currently active one.
+     * If replacement is enabled, it replaces all the furniture types named
+     * identically, with the other catalog's ones
+     * @param catalogName The name of the other furniture type catalog
+     * @param replace Turns on or off the replacement option for the merge
+     */
     @Override
     public void merge(String catalogName, boolean replace) {
         super.merge(catalogName,replace);
@@ -61,19 +76,32 @@ public class FurnitureTypesCatalogController
         notify(new FTCatalogCheckoutEvent(getNameActiveCatalog()));
     }
     
+    /**
+     * Switches the active catalog to the one specified
+     * @param catalogName The name of the furniture type catalog to activate
+     */
     @Override
     public void checkout(String catalogName) {
         super.checkout(catalogName);
         notify(new FTCatalogCheckoutEvent(catalogName));
     }
     
+    /**
+     * Loads a furniture type catalog (previously saved) from disk
+     * @param path The absolute or relative directory path
+     * @throws JAXBException 
+     */
     @Override
     public void load(String path) throws JAXBException {
         super.load(path);
         notify(new FTCatalogSetModifiedEvent(lastLoaded, true));
     }
     
-    
+    /**
+     * Obtains all the selected furniture types. This function is used in order to
+     * determine whether performing a checkout operation leaves some selected furniture unresolved
+     * @return A collection of strings containing all the currently selected furniture type in the wish list
+     */
     public Collection<String> getSelectedFurnitureTypes()
     {
         return getWishList().getElementTypes();
